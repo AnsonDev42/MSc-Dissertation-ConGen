b888d84d1139e92ac2dc248a70eb82e1c342081e
Anson <tmmanson77@gmail.com>
2023-05-30 17:30:22 -0700

    fix dataloader dim error for batch by [bc,1, 160,192,160]
    handle file non-exists error

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,6 @@
 import os
+
+import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
@@ -9,42 +11,49 @@ import h5py
 class CustomDataset(Dataset):
     def __init__(self, root_dir, csv_file):
         self.root_dir = root_dir
-        self.data_info = pd.read_csv(csv_file)
+        data_info = pd.read_csv(csv_file)
+        self.data_info = data_info[data_info.apply(lambda row: os.path.exists(self._get_h5_path(row)), axis=1)]
 
     def __len__(self):
         return len(self.data_info)
+
+    def _get_h5_path(self, row):
+        participant_id = row['participant_id']
+        study = row['study']
+        h5_path = os.path.join(self.root_dir, study, str(participant_id))
+        h5_path = h5_path + f'/{participant_id}_T1w.h5'
+        return h5_path
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        # Get the age from the csv file
         age = self.data_info.iloc[idx]['age']
-
-        # Get the participant_id from the csv file
         participant_id = self.data_info.iloc[idx]['participant_id']
-
-        # Construct the path of the .h5 file
+        status = self.data_info.iloc[idx]['status']
         # Assuming the structure is '{dataset_name}/ds{6digits}/sub-{participant_id}'
+
         study = self.data_info.iloc[idx]['study']  # e.g. 'AOMIC/ds002785'
         h5_path = os.path.join(self.root_dir, study,
                                str(participant_id))  # e.g. 'data/preprocessed/AOMIC/ds002785/sub-0001'
         h5_path = h5_path + f'/{participant_id}_T1w.h5'  # e.g. 'data/preprocessed/AOMIC/ds002785/sub-0001/sub-0001_T1w.h5'
 
         # Load the .h5 file
-        sample = {'h5_data': 0, 'age': age, 'root_dir': self.root_dir, 'study': study,
-                  'participant_id': participant_id}
+        sample = {'age': age, 'root_dir': self.root_dir, 'study': study,
+                  'participant_id': participant_id, 'status': status}
         try:
 
             with h5py.File(h5_path, 'r') as f:
-                sample['h5_data'] = f['preprocessed_volume'][:]
-
+                data = f['preprocessed_volume'][:]
+                data = np.expand_dims(data, axis=0)
+                sample['h5_data'] = torch.from_numpy(data)
             return sample
-        except:  # TODO: specify the exception
+        except FileNotFoundError:
             print('File not found, load useless f for further check.' + h5_path)
-            sample['age'] = -1  # set flag for further check in label_BAG.py
-            sample['h5_data'] = torch.full((160, 192, 160), -1)  # dummy tensor
-            return sample
+            return None
+            # sample['age'] = -1  # set flag for further check in label_BAG.py
+            # sample['h5_data'] = torch.full((160, 192, 160), -1)  # dummy tensor
+            # return sample
             # with h5py.File('data/preprocessed/AOMIC/ds002785/sub-0001/sub-0001_T1w.h5', 'r') as f:
             #     h5_data = f['preprocessed_volume'][:]
             # sample = {'h5_data': h5_data, 'age': age, 'root_dir': self.root_dir, 'study': study,
@@ -56,6 +65,6 @@ if __name__ == '__main__':
     dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
     dataloader_iter = iter(dataloader)
     sample = next(dataloader_iter)
-    assert sample['h5_data'].shape == (4, 160, 192, 160)
+    assert sample['h5_data'].shape == (4, 1, 160, 192, 160), f"{sample['h5_data'].size()}"
     print(sample['age'])
     print(sample['participant_id'])
