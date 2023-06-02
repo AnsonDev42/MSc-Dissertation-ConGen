da8d1abfed7304c66c7d655dc07032d2dd420adb
Anson <tmmanson77@gmail.com>
2023-06-02 12:38:24 -0700

    WIP my CVAE

--- contrastive_vae/my_vae_utils.py
+++ contrastive_vae/my_vae_utils.py
@@ -31,7 +31,7 @@ class Encoder(nn.Module):
     def __init__(self, input_dim, intermediate_dim, latent_dim):
         super(Encoder, self).__init__()
         # hidden layers
-        self.z_h_layers = DenseLayers([input_dim] + intermediate_dim) #line143 in vae_utils.py
+        self.z_h_layers = DenseLayers(input_dim + intermediate_dim) #line143 in vae_utils.py
         self.z_mean_layer = nn.Linear(intermediate_dim[-1], latent_dim)
         self.z_log_var_layer = nn.Linear(intermediate_dim[-1], latent_dim)
         self.sampler = Sampler()
@@ -54,20 +54,59 @@ class Decoder(nn.Module):
 
 
 class ContrastiveVAE(nn.Module):
-    def __init__(self, input_dim, intermediate_dim, latent_dim):
+    def __init__(self, input_dim, intermediate_dim, latent_dim, beta=1,disentangle=False, gamma=0):
         super(ContrastiveVAE, self).__init__()
+        # add input layer before encoder
+        self.input_layer = nn.Linear(input_dim, input_dim)
+        if isinstance(intermediate_dim, int):
+            intermediate_dim = [intermediate_dim]
         self.z_encoder = Encoder(input_dim, intermediate_dim, latent_dim)
         self.s_encoder = Encoder(input_dim, intermediate_dim, latent_dim)
         self.decoder = Decoder(2 * latent_dim, intermediate_dim, input_dim)
+        self.disentangle = disentangle
+        self.beta = beta
+        self.gamma = gamma
+        
+        
+        if self.disentangle: # line 209 in vae_utils.py
+            self.discriminator = nn.Linear(2 * latent_dim, 1)
+            self.sigmoid = nn.Sigmoid()
 
     def forward(self, tg_inputs, bg_inputs):
         tg_z_mean, tg_z_log_var, tg_z = self.z_encoder(tg_inputs)
         tg_s_mean, tg_s_log_var, tg_s = self.s_encoder(tg_inputs)
         bg_s_mean, bg_s_log_var, bg_s = self.s_encoder(bg_inputs)
 
-        tg_outputs = self.decoder(torch.cat((tg_z, tg_s), dim=-1))
+        tg_outputs = self.decoder(torch.cat((tg_z, tg_s), dim=-1)) #line 196 in vae_utils.py
         bg_outputs = self.decoder(torch.cat((torch.zeros_like(tg_z), bg_s), dim=-1))
+        fg_outputs = self.decoder(torch.cat((tg_z, torch.zeros_like(tg_z)), dim=-1))
 
-        return tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean, tg_s_log_var, bg_s_mean, bg_s_log_var
+
+        if self.disentangle:
+            batch_size = tg_inputs.size(0)
+            z1 = tg_z[:batch_size // 2, :]
+            z2 = tg_z[batch_size // 2:, :]
+            s1 = tg_s[:batch_size // 2, :]
+            s2 = tg_s[batch_size // 2:, :]
+            q_bar = torch.cat(
+                [torch.cat([s1, z2], dim=1), 
+                 torch.cat([s2, z1], dim=1)], dim=0)
+            q = torch.cat( #line 219 in vae_utils.py
+                [torch.cat([s1, z1], dim=1), 
+                 torch.cat([s2, z2], dim=1)], dim=0)
+            
+            q_bar_score = self.sigmoid(self.discriminator(q_bar))
+            q_score = self.sigmoid(self.discriminator(q))        
+            tc_loss = torch.log(q_score / (1 - q_score))
+            discriminator_loss = -torch.log(q_score) - torch.log(1 - q_bar_score)
+            # the reconstruction_loss is not complete, need to add when training
+            return (
+                tg_outputs, bg_outputs,
+                tg_z_mean, tg_z_log_var,
+                tg_s_mean, tg_s_log_var,
+                bg_s_mean, bg_s_log_var,
+                tc_loss, discriminator_loss)
+        else:
+          return tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean, tg_s_log_var, bg_s_mean, bg_s_log_var
 
 
