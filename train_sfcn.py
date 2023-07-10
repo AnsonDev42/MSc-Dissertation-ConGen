0001914a19da9e86f3e8848efc2dd6b682524bf8
Anson <tmmanson77@gmail.com>
2023-07-10 18:11:10 +0100

    WIP loss error

--- train_sfcn.py
+++ train_sfcn.py
@@ -15,8 +15,21 @@ from label_BAG import sfcn_loader
 from dp_model import dp_loss as dpl
 import nibabel as nib
 from dp_model import dp_utils as dpu
+from sfcn_helper import get_bin_range_step
 
 writer = SummaryWriter('runs/experiment_name')
+
+
+def transform_labels_to_distribution(labels_batch, sigma, device):
+    y_batch = []
+
+    for label in labels_batch:
+        label = label.item()
+        bin_range, bin_step = get_bin_range_step(label)
+        y, _ = dpu.num2vect(label, bin_range, bin_step, sigma)
+        y_batch.append(y)
+    y_batch = np.array(y_batch)
+    return torch.Tensor(y_batch).to(dtype=torch.float32, device=device)
 
 
 def train_sfcn():
@@ -38,7 +51,7 @@ def train_sfcn():
     csv_file = 'data/filtered_mdd_db_age.csv'
 
     init_learning_rate = 0.01
-    epochs = 1
+    epochs = 2
     weight_decay = 0.001
     batch_size = 8  # adjust as the paper
 
@@ -46,10 +59,10 @@ def train_sfcn():
     healthy_dataset = DataStoreDataset(root_dir, csv_file, )
     healthy_dataset.load_data_info(root_dir, csv_file, filter_func=filter_healthy)
     # split to train and test
-    train_size = int(0.8 * len(healthy_dataset))
+    train_size = int(0.95 * len(healthy_dataset))
     test_size = len(healthy_dataset) - train_size
     train_dataset, test_dataset = torch.utils.data.random_split(healthy_dataset, [train_size, test_size])
-    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn,
+    dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn,
                             num_workers=4)
     dataloader_val = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn,
                                 num_workers=4)
@@ -60,9 +73,6 @@ def train_sfcn():
     best_loss = np.inf  # Initialize the best loss to infinity
     best_epoch = 0  # Initialize the best epoch to zero
 
-    # To store validation MAE for each epoch
-    val_mae = []
-
     # Training loop
     for epoch in range(epochs):
         running_loss = 0.0
@@ -70,46 +80,34 @@ def train_sfcn():
             if batch is None:
                 continue
                 # Get the inputs
-            inputs = []
-            labels = batch['age']
-            inputs = batch['image_data']
-            labels = labels.to(dtype=torch.float32, device=device)
-            # Convert the paths into actual image data
-            inputs = np.array(inputs)
-            inputs = torch.Tensor(inputs)
-            inputs = inputs.to(dtype=torch.float32, device=device)
-            labels = labels.to(dtype=torch.float32, device=device)
+            labels = transform_labels_to_distribution(batch['age'], sigma=1, device=device)
+            # create a dummy label filled with 0 in torch tensor format
 
-            tmp_dirs = batch['tmp_dir']
-            # check the shape of inputs and labels
-            # inputs = inputs.to(dtype=torch.float32, device=device)
-            # assert labels.shape == (batch_size), labels.shape
-            # Zero the parameter gradients
+            inputs = torch.Tensor(batch['image_data']).to(dtype=torch.float32, device=device)
+
             optimizer.zero_grad()
-
-            # Forward + backward + optimize
             outputs = sfcn.module(inputs)
-            # output is a list
-            output_tensor = outputs[0]
-            # output_tensor = output_tensor.reshape([batch_size, -1])
-            # use it in the loss computation
+            output_tensor = outputs[0].reshape([batch['age'].shape[0], -1])
             loss = dpl.my_KLDivLoss(output_tensor, labels)
-            writer.add_scalar('Training Loss', loss, global_step=i + epoch * len(dataloader))
-            # outputs_shaped = outputs.reshape([batch_size, -1])
-            # x = outputs.reshape([batch_size, -1])
-            # loss = dpl.my_KLDivLoss(outputs, labels).numpy()
+            if i % 10 == 9:  # print every 500 mini-batches
+                print(f"loss: {np.round(loss.item(), 6)} [{i + 1}/{len(dataloader)}]")
+            writer.add_scalar('Training Loss', loss, i)
             loss.backward()
             optimizer.step()
 
+            tmp_dirs = batch['tmp_dir']
             # remove the temporary files
             for tmp_dir in tmp_dirs:
                 if tmp_dir is not None:
                     shutil.rmtree(tmp_dir)
                     # print(f'Temporary directory removed: {tmp_dir}')
+        running_loss += loss.item()
+        # print statistics
+        print(f'Epoch: {epoch + 1}/{epochs}, Loss: {running_loss / len(dataloader):.4f}')
         scheduler.step()
 
         # Print statistics
-        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, loss.item()))
+        print(f'Epoch: {epoch + 1}/{epochs}, Loss: {loss.item():.4f}')
         writer.add_scalar('training loss', loss, epoch)
         # After training for one epoch, we evaluate the model on the validation set
         val_loss = 0.0
@@ -118,12 +116,9 @@ def train_sfcn():
             for i, batch in enumerate(dataloader_val, 0):
                 if batch is None:
                     continue
-                # Get the inputs
-                labels = batch['age']
+                # create np.array of the labels filled with zeros
+                labels = torch.zeros([batch_size, 1])
                 inputs = batch['image_data']
-                labels = labels.to(dtype=torch.float32, device=device)
-                # Convert the paths into actual image data
-                inputs = np.array(inputs)
                 inputs = torch.Tensor(inputs)
                 inputs = inputs.to(dtype=torch.float32, device=device)
                 labels = labels.to(dtype=torch.float32, device=device)
@@ -135,10 +130,10 @@ def train_sfcn():
                 # output_tensor = output_tensor.reshape([batch_size, -1])
                 # use it in the loss computation
                 loss = dpl.my_KLDivLoss(output_tensor, labels)
-
                 val_loss += loss.item()
 
-        val_loss = val_loss / len(dataloader_val)  # Calculate average validation loss for this epoch
+            avg_val_loss = val_loss / i  # Calculate average validation loss
+            writer.add_scalar('Average Validation Loss', avg_val_loss, epoch)
 
         # Check if this is the best model
         if val_loss < best_loss:
@@ -156,15 +151,16 @@ def train_sfcn():
             }, 'checkpoint.pth')
         sfcn.train()  # Set the model back to training mode for the next epoch
 
-        print('Minimum validation best_loss: ', min(best_loss))
+        print(f' best validation loss: {best_loss}')
     print('Finished Training')
     writer.close()
 
 
 if __name__ == '__main__':
+    # calculat the training time
+    print(f"start training")
     t_s = time.time()
-    print(f"start training{t_s}")
     train_sfcn()
     t_e = time.time()
     print(f"end training{t_e}")
-    print(f"total time: {t_e - t_s}")
+    print(f"total time in seconds: {t_e - t_s}")
