d98a907825eeb8d3a67e6f51c66b83b2cef58d41
Anson <tmmanson77@gmail.com>
2023-07-16 14:43:08 +0100

    updated model with lesser params

--- contrastive_vae/my_vae_utils.py
+++ contrastive_vae/my_vae_utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt
@@ -5,6 +7,10 @@ from torch.autograd import Variable
 import torch.optim as optim
 import numpy as np
 import torch.nn.functional as F
+import sys
+
+sys.path.append('/afs/inf.ed.ac.uk/user/s23/s2341683/pycharm_remote_tmp/ConGeLe')
+from dataloader import DataStoreDataset, filter_depressed, filter_healthy, custom_collate_fn
 
 
 class Sampler(nn.Module):
@@ -42,27 +48,32 @@ class Encoder(nn.Module):
         self.input_shape = input_shape
         self.intermediate_dim = intermediate_dim
         self.latent_dim = latent_dim
-        print(f"input_shape is {input_shape}")
+        # print(f"input_shape is {input_shape}")
         # since the input shape would be 1*160*192*160, where 1 is the input channel number
-        self.z_conv1 = nn.Conv3d(input_shape[0], filters * 2, kernel_size, stride=2, padding=(1, 1, 1), bias=bias)
-        self.z_conv2 = nn.Conv3d(filters * 2, filters * 4, kernel_size, stride=2, padding=(1, 1, 1), bias=bias)
-
+        kernel_size = 2
+        # stride =2
+        self.z_conv1 = nn.Conv3d(input_shape[0], filters * 2, kernel_size=12, stride=11, padding=(1, 1, 1), bias=bias)
+        self.z_conv2 = nn.Conv3d(filters * 2, filters * 4, kernel_size=4, stride=3, padding=(1, 1, 1), bias=bias)
         # hidden layers
         # self.z_h_layers = DenseLayers([input_shape, intermediate_dim])  # line143 in vae_utils.py
-        self.z_h_layers = nn.Linear(10543232, intermediate_dim)
+        self.z_h_layers = nn.Linear(19200, intermediate_dim)
         self.z_mean_layer = nn.Linear(intermediate_dim, latent_dim)
         self.z_log_var_layer = nn.Linear(intermediate_dim, latent_dim)
         self.sampler = Sampler()
+        # print each layers parameters
+        # for name, param in self.named_parameters():
+        #     print(f"Encoder layer name: {name}, size: {param.size()}")
 
     def forward(self, x):
         x = F.relu(self.z_conv1(x))
         x = F.relu(self.z_conv2(x))
+        # x = F.relu(self.z_conv3(x))
 
         z_shape = list(x.size())
         print(f"shape of x is {z_shape}")  # shape of x is [ 128, 40, 48, 40]
-        x = x.flatten(start_dim=1)
-        print(f"shape of x is {x.size()}")  # shape of x is 314,572,800
-        z_h = self.z_h_layers(x)
+        z_h = x.flatten(start_dim=1)
+        print(f"shape of x is {z_h.size()}")  # shape of x is 314,572,800
+        z_h = self.z_h_layers(z_h)
         z_mean = self.z_mean_layer(z_h)
         z_log_var = self.z_log_var_layer(z_h)
         z = self.sampler((z_mean, z_log_var))
@@ -70,10 +81,12 @@ class Encoder(nn.Module):
         return z_mean, z_log_var, z, z_shape
 
 
-# tg_inputs = torch.randn(1,1, 160, 192, 160)
-# encoder= Encoder(input_shape=(1,160,192,160), intermediate_dim=128, latent_dim=2,filters=32, kernel_size=3, bias=True)
-# res = encoder(tg_inputs)
-# print(f"z_shape {res[3]}") # z_shape [1, 128, 40, 48, 40]
+tg_inputs = torch.randn(1, 1, 160, 192, 160)
+encoder = Encoder(input_shape=(1, 160, 192, 160), intermediate_dim=128, latent_dim=2, filters=32, kernel_size=3,
+                  bias=True)
+res = encoder(tg_inputs)
+print(f"!!!!z_shape {res[3]}")  # z_shape [1, 128, 40, 48, 40]
+
 
 class Decoder(nn.Module):
     """
@@ -113,7 +126,8 @@ class Decoder(nn.Module):
                                              out_channels=1,
                                              kernel_size=self.kernel_size,
                                              stride=1,
-                                             padding=2,
+                                             padding=4,
+                                             output_padding=0,
                                              bias=bias
                                              )
         self.sigmoid = nn.Sigmoid()
@@ -139,8 +153,8 @@ class ContrastiveVAE(nn.Module):
                  gamma=1, bias=True, batch_size=64, device=None):
         super(ContrastiveVAE, self).__init__()
         # image_size, _, _, channels = input_shape
-        kernel_size = 2  # 3 to 2
-        filters = 32
+        kernel_size = 3  # 3 to 2
+        filters = 32  # 32
         nlayers = 2
         self.z_encoder = Encoder(input_shape, intermediate_dim, latent_dim, filters, kernel_size, bias=bias)
         self.s_encoder = Encoder(input_shape, intermediate_dim, latent_dim, filters, kernel_size, bias=bias)
@@ -164,21 +178,23 @@ class ContrastiveVAE(nn.Module):
 
     def forward(self, tg_inputs, bg_inputs):
         tg_z_mean, tg_z_log_var, tg_z, shape_z = self.z_encoder(tg_inputs)
-        print("tg_z shape", tg_z.shape)
+        # print("tg_z shape", tg_z.shape)
         tg_s_mean, tg_s_log_var, tg_s, shape_s = self.s_encoder(tg_inputs)
         bg_z_mean, bg_z_log_var, bg_z, _ = self.z_encoder(
             bg_inputs)  # s->z:conflict with example code but match with paper
-        print(f"input of decoders: {torch.cat((tg_z, tg_s), dim=-1).shape}")
+        # print(f"input of decoders: {torch.cat((tg_z, tg_s), dim=-1).shape}")
         tg_outputs = self.decoder(torch.cat((tg_z, tg_s), dim=-1))  # line 196 in vae_utils.py
         bg_outputs = self.decoder(torch.cat((torch.zeros_like(tg_z), bg_z), dim=-1))
-        fg_outputs = self.decoder(torch.cat((tg_z, torch.zeros_like(tg_z)), dim=-1))
+        # fg_outputs = self.decoder(torch.cat((tg_z, torch.zeros_like(tg_z)), dim=-1))
 
         if self.disentangle:
             batch_size = tg_inputs.size(0)
+            print(f"tg_s.size(), bg_z.size() {tg_s.size(), bg_z.size()}")
             z1 = tg_z[:batch_size // 2, :]
             z2 = tg_z[batch_size // 2:, :]
             s1 = tg_s[:batch_size // 2, :]
             s2 = tg_s[batch_size // 2:, :]
+            print(f"s1.size(), z2.size() {s1.size(), z2.size()}")
             q_bar = torch.cat(
                 [torch.cat([s1, z2], dim=1),
                  torch.cat([s2, z1], dim=1)],
@@ -218,15 +234,15 @@ def cvae_loss(original_dim,
               tg_z_mean, tg_z_log_var,
               tg_s_mean, tg_s_log_var,
               bg_z_mean, bg_z_log_var,
-              beta=1.0, disentangle=False, gamma=0.0, tc_loss=None, discriminator_loss=None):
+              beta=1.0, disentangle=True, gamma=1.0, tc_loss=None, discriminator_loss=None):
     # Reconstruction Loss
     reconstruction_loss = nn.MSELoss(reduction="none")
     if tg_outputs.is_cuda:
         print('tg_outputs is cuda, move outputs to device')
         tg_inputs = tg_inputs.to(tg_outputs.device)
         bg_inputs = bg_inputs.to(bg_outputs.device)
-    tg_reconstruction_loss = reconstruction_loss(tg_inputs, tg_outputs).view(tg_inputs.size(0), -1).sum(dim=1)
-    bg_reconstruction_loss = reconstruction_loss(bg_inputs, bg_outputs).view(bg_inputs.size(0), -1).sum(dim=1)
+    tg_reconstruction_loss = reconstruction_loss(tg_outputs, tg_inputs).view(tg_inputs.size(0), -1).sum(dim=1)
+    bg_reconstruction_loss = reconstruction_loss(bg_outputs, bg_inputs).view(bg_inputs.size(0), -1).sum(dim=1)
     reconstruction_loss = 0.5 * (tg_reconstruction_loss + bg_reconstruction_loss) \
                           / (original_dim[1] * original_dim[2] * original_dim[3])
 
@@ -247,7 +263,6 @@ def cvae_loss(original_dim,
         print(f"discriminator_loss {discriminator_loss}")
         print(f"tc_loss {tc_loss}")
         print(f"kl_loss {kl_loss.mean()}")
-
     else:
         cvae_loss = reconstruction_loss.mean() + beta * kl_loss.mean()
         # print each loss
@@ -274,39 +289,111 @@ def plot_latent_space(tg_z_mean, bg_z_mean, y=None, plot=True, name='z_mean'):
 
 
 if __name__ == '__main__':
+    # current time in ddmm_hhmm format
+    now = datetime.datetime.now()
+    time_str = now.strftime("%d%m_%H%M")
+    torch.cuda.empty_cache()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device_ids = [0, 3, 4]
+    # print(f'Using device: {device} and potential device_ids: {device_ids}')
+    # Define hyperparameters
+    learning_rate = 0.001
+    epochs = 100
+
+    # HOME = os.environ['HOME']
+    # root_dir = f'{HOME}/GenScotDepression/data/ukb/imaging/raw/t1_structural_nifti_20252'
+    # # csv_file = './../data/filtered_mdd_db_age.csv'
+    # csv_file = f'./../brain_age_info_retrained_sfcn_bc_filtered.csv'
+    # healthy_dataset = DataStoreDataset(root_dir, csv_file, on_the_fly=False)
+    # healthy_dataset.load_data_info(root_dir, csv_file, filter_func=filter_healthy)
+    # mdd_dataset = DataStoreDataset(root_dir, csv_file, on_the_fly=False)
+    # mdd_dataset.load_data_info(root_dir, csv_file, filter_func=filter_depressed)
+    #
+    # hc_train_size = int(0.8 * len(healthy_dataset))
+    # hc_val_size = len(healthy_dataset) - hc_train_size
+    # hc_train_dataset, hc_test_dataset = torch.utils.data.random_split(healthy_dataset, [hc_train_size, hc_val_size])
+    #
+    # mdd_train_size = int(0.8 * len(mdd_dataset))
+    # mdd_val_size = len(mdd_dataset) - mdd_train_size
+    # mdd_train_dataset, mdd_test_dataset = torch.utils.data.random_split(mdd_dataset, [mdd_train_size, mdd_val_size])
+    #
+    # tg_train_data = mdd_train_dataset
+    # bg_train_data = hc_train_dataset
+    #
+    # tg_test_data = mdd_test_dataset
+    # bg_test_data = hc_test_dataset
+    #
+    # # Split the data into batches
+    # tg_train_loader = torch.utils.data.DataLoader(tg_train_data, batch_size=batch_size, shuffle=True,
+    #                                               collate_fn=custom_collate_fn,
+    #                                               num_workers=8)
+    # bg_train_loader = torch.utils.data.DataLoader(bg_train_data, batch_size=batch_size, shuffle=True,
+    #                                               collate_fn=custom_collate_fn,
+    #                                               num_workers=8)
+    #
+    # tg_test_loader = torch.utils.data.DataLoader(tg_test_data, batch_size=batch_size, shuffle=False,
+    #                                              collate_fn=custom_collate_fn,
+    #                                              num_workers=8)
+    # bg_test_loader = torch.utils.data.DataLoader(bg_test_data, batch_size=batch_size, shuffle=False,
+    #                                              collate_fn=custom_collate_fn,
+    #                                              num_workers=8)
+
+    # best_loss = np.inf  # Initialize the best loss to infinity
+    # best_epoch = 0  # Initialize the best epoch to zero
+    # epoch_number = 0  # Initialize the epoch number to zero
+    # for epoch in range(epochs):
+    #     # Loop over the batches of data
+    #     assert len(tg_train_loader) == len(bg_train_loader)  # TODO: check if this is true
+    #     print('EPOCH {}:'.format(epoch_number + 1))
+    #     running_loss = 0.0
+    #     last_loss = 0.0
+    #     num_batches = 0
+    #     for i, batch in enumerate(zip(tg_train_loader, bg_train_loader)):
+    #         if batch is None:
+    #             continue
+    #         tg_inputs, bg_inputs = batch[0]['image_data'].to(dtype=torch.float32), batch[1]['image_data'].to(
+    #             dtype=torch.float32)
+    #
+    #         assert tg_inputs.shape == bg_inputs.shape
+    #     break
+    # exit(0)
     # device = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
     # tg_inputs = torch.randn(8, 1, 160, 192, 160).to(device)
     # bg_inputs = torch.randn(8, 1, 160, 192, 160).to(device)
     # cvae = ContrastiveVAE()
     # cvae.to(device)
-    torch.cuda.empty_cache()
+    # torch.cuda.empty_cache()
 
-    device = torch.device("cuda:4")
     # device_ids = [4, 1]
     print(f'Using device: {device}')
     # Define hyperparameters
     learning_rate = 0.001
-    num_epochs = 10
-    batch_size = 8  # from 32
-    # Instantiate the model
     input_dim = (1, 160, 192, 160)  # 784
-    intermediate_dim = 32  # 256
+    intermediate_dim = 128  # 256
     latent_dim = 2
     beta = 1
-    disentangle = False
+    disentangle = True
     gamma = 0
     model = ContrastiveVAE(input_dim, intermediate_dim, latent_dim, beta, disentangle, gamma)
-    model = nn.DataParallel(model, device_ids=[4, 1])  # Wrap the model with DataParallel
+    # model = nn.DataParallel(model, device_ids=[4, 1])  # Wrap the model with DataParallel
     model.to(device)
     pytorch_total_params = sum(p.numel() for p in model.parameters())
     print(f"Number of parameters: {pytorch_total_params}")
+    # num of parameters in z_encoder
+    z_encoder_params = sum(p.numel() for p in model.z_encoder.parameters())
+    print(f"Number of parameters in z_encoder: {z_encoder_params}")
+
     pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Number of trainable parameters: {pytorch_total_params}")
-    model = torch.compile(model)
-    tg_inputs = torch.randn(8, 1, 160, 192, 160)
-    bg_inputs = torch.randn(8, 1, 160, 192, 160)
-    tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean, tg_s_log_var, bg_z_mean, bg_z_log_var = model(tg_inputs,
-                                                                                                              bg_inputs)
+    # model = torch.compile(model)
+    tg_inputs = torch.randn(8, 1, 160, 192, 160).to(dtype=torch.float32)
+    bg_inputs = torch.randn(8, 1, 160, 192, 160).to(dtype=torch.float32)
+    tg_inputs = tg_inputs.to(device)
+    bg_inputs = bg_inputs.to(device)
+    tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean, tg_s_log_var, bg_z_mean, bg_z_log_var, \
+        tc_loss, discriminator_loss = model(tg_inputs,
+                                            bg_inputs)
+
     print(f"tg_outputs shape {tg_outputs.shape}")
     print(f"bg_outputs shape {bg_outputs.shape}")
     print(f"tg_z_mean shape {tg_z_mean.shape}")
@@ -316,4 +403,8 @@ if __name__ == '__main__':
     print(f"bg_z_mean shape {bg_z_mean.shape}")
 
     loss = cvae_loss(input_dim, tg_inputs, bg_inputs, tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean,
-                     tg_s_log_var, bg_z_mean, bg_z_log_var, beta, disentangle, gamma)
+                     tg_s_log_var, bg_z_mean, bg_z_log_var, beta, disentangle, gamma, tc_loss, discriminator_loss)
+    del tg_outputs, bg_outputs, tg_z_mean, tg_z_log_var, tg_s_mean, tg_s_log_var, bg_z_mean, bg_z_log_var
+    torch.cuda.empty_cache()
+    loss.backward()
+    print(f"loss: {loss}")
